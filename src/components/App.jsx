14cff7b207941298acc5fdac351f91364395dd79
YuDo-fsd <y.doloban@gmail.com>
2023-05-23 23:33:47 +0200

    changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,59 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: '',
+  };
+
+  handleInput = e => {
+    const { name, value } = e.currentTarget;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  render() {
+    const { contacts, name, number } = this.state;
+
+    return (
+      <div>
+        <h2>Phonebook</h2>
+        <form action="submit">
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              value={name}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              onChange={this.handleInput}
+              // required
+            />
+          </label>
+          <br />
+          <label>
+            Phone
+            <input
+              type="tel"
+              name="number"
+              value={number}
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              onChange={this.handleInput}
+              // required
+            />
+          </label>
+          <br />
+          <button>Send</button>
+        </form>
+        <h2>Contacts</h2>
+        <ul>
+          <li>{contacts.name}</li>
+        </ul>
+      </div>
+    );
+  }
+}
